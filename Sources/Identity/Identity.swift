b587f94e1ee69868502e3209c0818709434db435
lyzkov <lyzkov@gmail.com>
2019-07-03 13:43:58 +0200

    Improved naming

--- Sources/Identity/Identity.swift
+++ Sources/Identity/Identity.swift
@@ -13,22 +13,22 @@ import Foundation
 /// defaults to String.
 public protocol Identifiable {
     /// The backing raw type of this type's identifier.
-    associatedtype RawIdentifier = String
+    associatedtype RawRepresentation = String
     /// Shorthand type alias for this type's identifier.
     typealias ID = Identifier<Self>
     /// The ID of this instance.
     var id: ID { get }
 }
 
-/// A type-safe identifier for a given `Value`, backed by a raw value.
+/// A type-safe identifier for a given Entity, backed by a raw value.
 /// When backed by a `Codable` type, `Identifier` also becomes codable,
 /// and will be encoded into a single value according to its raw value.
-public struct Identifier<Value: Identifiable> {
+public struct Identifier<Entity: Identifiable> {
     /// The raw value that is backing this identifier.
-    public let rawValue: Value.RawIdentifier
+    public let rawValue: Entity.RawRepresentation
 
     /// Initialize an instance with a raw value.
-    public init(rawValue: Value.RawIdentifier) {
+    public init(rawValue: Entity.RawRepresentation) {
         self.rawValue = rawValue
     }
 }
@@ -36,8 +36,8 @@ public struct Identifier<Value: Identifiable> {
 // MARK: - Integer literal support
 
 extension Identifier: ExpressibleByIntegerLiteral
-          where Value.RawIdentifier: ExpressibleByIntegerLiteral {
-    public init(integerLiteral value: Value.RawIdentifier.IntegerLiteralType) {
+          where Entity.RawRepresentation: ExpressibleByIntegerLiteral {
+    public init(integerLiteral value: Entity.RawRepresentation.IntegerLiteralType) {
         rawValue = .init(integerLiteral: value)
     }
 }
@@ -45,37 +45,37 @@ extension Identifier: ExpressibleByIntegerLiteral
 // MARK: - String literal support
 
 extension Identifier: ExpressibleByUnicodeScalarLiteral
-          where Value.RawIdentifier: ExpressibleByUnicodeScalarLiteral {
-    public init(unicodeScalarLiteral value: Value.RawIdentifier.UnicodeScalarLiteralType) {
+          where Entity.RawRepresentation: ExpressibleByUnicodeScalarLiteral {
+    public init(unicodeScalarLiteral value: Entity.RawRepresentation.UnicodeScalarLiteralType) {
         rawValue = .init(unicodeScalarLiteral: value)
     }
 }
 
 extension Identifier: ExpressibleByExtendedGraphemeClusterLiteral
-          where Value.RawIdentifier: ExpressibleByExtendedGraphemeClusterLiteral {
-    public init(extendedGraphemeClusterLiteral value: Value.RawIdentifier.ExtendedGraphemeClusterLiteralType) {
+          where Entity.RawRepresentation: ExpressibleByExtendedGraphemeClusterLiteral {
+    public init(extendedGraphemeClusterLiteral value: Entity.RawRepresentation.ExtendedGraphemeClusterLiteralType) {
         rawValue = .init(extendedGraphemeClusterLiteral: value)
     }
 }
 
 extension Identifier: ExpressibleByStringLiteral
-          where Value.RawIdentifier: ExpressibleByStringLiteral {
-    public init(stringLiteral value: Value.RawIdentifier.StringLiteralType) {
+          where Entity.RawRepresentation: ExpressibleByStringLiteral {
+    public init(stringLiteral value: Entity.RawRepresentation.StringLiteralType) {
         rawValue = .init(stringLiteral: value)
     }
 }
 
 // MARK: - Compiler-generated protocol support
 
-extension Identifier: Equatable where Value.RawIdentifier: Equatable {}
-extension Identifier: Hashable where Value.RawIdentifier: Hashable {}
+extension Identifier: Equatable where Entity.RawRepresentation: Equatable {}
+extension Identifier: Hashable where Entity.RawRepresentation: Hashable {}
 
 // MARK: - Codable support
 
-extension Identifier: Codable where Value.RawIdentifier: Codable {
+extension Identifier: Codable where Entity.RawRepresentation: Codable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
-        rawValue = try container.decode(Value.RawIdentifier.self)
+        rawValue = try container.decode(Entity.RawRepresentation.self)
     }
 
     public func encode(to encoder: Encoder) throws {
